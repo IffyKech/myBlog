f14535219dcc91453be81535f48db78d5c65599d
IffyKech <iffykech@gmail.com>
2020-12-04 21:42:52 +0000

    Added condition flows for inputs

--- static/scripts/render.js
+++ static/scripts/render.js
@@ -3,17 +3,29 @@ function createUser() {
     let password = document.getElementById("registerPassword").value;
     let repeat_password = document.getElementById("registerRepeat").value;
 
-    let xmlhttp = new XMLHttpRequest();
-
-    xmlhttp.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-            alert("Account Created");
-        }
+    if (username.length < 1 || password.length < 1 || repeat_password.length < 1) { // if a value has not been entered in every field
+        alert("Please Enter a value in all fields"); // inform the user
     }
 
-    xmlhttp.open("POST", "http://127.0.0.1:5000/login?username=" +username.toString()+ "&password=" +password.toString()+
-    "&repeat_password=" +repeat_password.toString());
-    xmlhttp.send();
+    else { // if a value has been entered in every field
+        if (repeat_password !== password) { // if the user did not re-enter the correct password
+            alert("Both Passwords did not match.<br>Please Try again.");
+        }
+
+        else { // if the user re-entered the correct password
+            let xmlhttp = new XMLHttpRequest();
+
+            xmlhttp.onreadystatechange = function () {
+                if (this.readyState === 4 && this.status === 200) {
+                    alert("Account Created");
+                }
+            }
+
+            xmlhttp.open("POST", "http://127.0.0.1:5000/login?username=" +username.toString()+ "&password=" +password.toString()+
+                "&repeat_password=" +repeat_password.toString());
+            xmlhttp.send();
+        }
+    }
 
 }
 
