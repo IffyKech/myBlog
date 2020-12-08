be2354b7fce38ac1c0c1c7072960b788dcb725f7
IffyKech <iffykech@gmail.com>
2020-12-08 22:01:05 +0000

    Added code to display error message if no post found

--- static/scripts/render-search.js
+++ static/scripts/render-search.js
@@ -1,58 +1,66 @@
 var results = json_results;
 var amountOfResults = results.length;
-/*
+var contentDiv = document.getElementById("content");
+if (amountOfResults > 1) {  // if the query returned results
+    /*
 Repeat the process of creating html for the posts, for as many posts as there are
  */
-var content_div = document.getElementById("content");
-for (let repeat = 0; repeat < amountOfResults; repeat++) {
-    /* Get the Post details iteratively for each post */
-    resultPostID = results[repeat][0];
-    resultPostTitle = results[repeat][1];
-    resultPostDate = results[repeat][2];
-    resultUserID = results[repeat][3];
-    resultUsername = results[repeat][4];
+    for (let repeat = 0; repeat < amountOfResults; repeat++) {
+        /* Get the Post details iteratively for each post */
+        let resultPostID = results[repeat][0];
+        let resultPostTitle = results[repeat][1];
+        let resultPostDate = results[repeat][2];
+        let resultUserID = results[repeat][3];
+        let resultUsername = results[repeat][4];
 
-    /* Create the new HTML elements for the posts */
-    /* POST TITLE */
-    var postDiv = document.createElement("div");
-    postDiv.className = "post";
+        /* Create the new HTML elements for the posts */
+        /* POST TITLE */
+        var postDiv = document.createElement("div");
+        postDiv.className = "post";
 
-    var postTitle = document.createElement("h2");
-    postTitle.className = "title";
-    postTitle.innerHTML = `<a href="http://127.0.0.1:5000/comments?id=${resultPostID}">${resultPostTitle}</a>`;
+        var postTitle = document.createElement("h2");
+        postTitle.className = "title";
+        postTitle.innerHTML = `<a href="http://127.0.0.1:5000/comments?id=${resultPostID}">${resultPostTitle}</a>`;
 
-    /* POST DATE */
-    var postDate = document.createElement("p");
-    postDate.className = "meta";
-    // TODO: replace the post id with the tag
-    postDate.innerHTML = `<span class="date">${resultPostDate}</span><span class="posted">Posted by: ${resultUsername}</a>`;
+        /* POST DATE */
+        var postDate = document.createElement("p");
+        postDate.className = "meta";
+        postDate.innerHTML = `<span class="date">${resultPostDate}</span><span class="posted">Posted by: ${resultUsername}</a>`;
 
-    /* WHITESPACE DIV */
-    var whitespaceDiv = document.createElement("div");
-    whitespaceDiv.style.clear = "both";
+        /* WHITESPACE DIV */
+        var whitespaceDiv = document.createElement("div");
+        whitespaceDiv.style.clear = "both";
 
-    /* READ MORE ENTRY */
-    var entryDiv = document.createElement("div");
-    entryDiv.className = "entry";
+        /* READ MORE ENTRY */
+        var entryDiv = document.createElement("div");
+        entryDiv.className = "entry";
 
-    var linksParagraph = document.createElement("p");
-    linksParagraph.className = "links";
-    linksParagraph.innerHTML = "<a href='http://127.0.0.1:5000/comments?id=${resultPostID}'>Read More</a>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;<a href='http://127.0.0.1:5000/comments?id=${resultPostID}'>Comments</a>";
+        var linksParagraph = document.createElement("p");
+        linksParagraph.className = "links";
+        linksParagraph.innerHTML = "<a href='http://127.0.0.1:5000/comments?id=${resultPostID}'>Read More</a>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;<a href='http://127.0.0.1:5000/comments?id=${resultPostID}'>Comments</a>";
 
-    /* append parent nodes to create html structure */
-    content_div.appendChild(postDiv);
+        /* append parent nodes to create html structure */
+        contentDiv.appendChild(postDiv);
 
-    // POST TITLE STRUCTURE
-    postDiv.appendChild(postTitle);
+        // POST TITLE STRUCTURE
+        postDiv.appendChild(postTitle);
 
-    // POST DATE STRUCTURE
-    postDiv.appendChild(postDate);
+        // POST DATE STRUCTURE
+        postDiv.appendChild(postDate);
 
-    // WHITESPACE DIV STRUCTURE
-    postDiv.appendChild(whitespaceDiv);
+        // WHITESPACE DIV STRUCTURE
+        postDiv.appendChild(whitespaceDiv);
 
-    // READ MORE ENTRY STRUCTURE
-    postDiv.appendChild(entryDiv);
-    entryDiv.appendChild(linksParagraph);
+        // READ MORE ENTRY STRUCTURE
+        postDiv.appendChild(entryDiv);
+        entryDiv.appendChild(linksParagraph);
 
+    }
+}
+
+else {  // if the query didn't return any results
+    var errorMessage = document.createElement("h1");
+    errorMessage.style.color = "red";
+    errorMessage.innerHTML = "No Posts Found";
+    contentDiv.appendChild(errorMessage);
 }
