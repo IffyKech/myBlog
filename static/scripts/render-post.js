d4283f1092f2b203f4918c71d416d89f1e4267fa
Iffy Kech <32880480+IffyKech@users.noreply.github.com>
2020-12-11 09:54:24 +0000

    Comment render-post.js

--- static/scripts/render-post.js
+++ static/scripts/render-post.js
@@ -1,8 +1,9 @@
-var postResults = jsonPostResults;
-var commentResults = jsonCommentResults;
-var amountOfComments = commentResults.length;
+var postResults = jsonPostResults;  // get the results of posts from the Flask response
+var commentResults = jsonCommentResults;  // get the results of comments from the Flask response
+var amountOfComments = commentResults.length;  // get the amount of comments
 
 /* RENDER POST */
+/* Set all attributes of post details */
 var postID = postResults[0][0];
 var tagID = postResults[0][1];
 var tagName = postResults[0][2];
@@ -12,6 +13,8 @@ var postDate = postResults[0][5];
 var creatorID = postResults[0][6];
 var creatorName = postResults[0][7];
 
+/* Start process of rendering the HTML post */
+/* These are the necessary elements that are needed in the HTML to render the post */
 var contentDiv = document.getElementById("content");
 
 var postDivEle = document.createElement("div");
@@ -40,7 +43,9 @@ var linksParagraphEle = document.createElement("p");
 linksParagraphEle.className = "links";
 linksParagraphEle.innerHTML = `Tag: ${tagName}`;
 
-contentDiv.appendChild(postDivEle);
+/* Once all the HTML elements are created, they need to be sorted in order of parent/child */
+
+contentDiv.appendChild(postDivEle);  // parent element to all the HTML tags for rendering the post
 
 postDivEle.appendChild(postTitleEle);
 postDivEle.appendChild(postDateEle);
@@ -51,13 +56,15 @@ entryDivEle.appendChild(linksParagraphEle);
 
 
 /* RENDER COMMENTS */
-if (amountOfComments > 0) { // if there were comments found
+if (amountOfComments > 0) { // if there were comments found from the Flask response
+    /* Create and render the HTML elements for each comment that was found in the Flask response */
     for (let repeat = 0; repeat < amountOfComments; repeat++) {
-
+        /* Retrieve the details of each comment */
         var commentID = commentResults[repeat][0];
         var commentContent = commentResults[repeat][1];
         var commentDate = commentResults[repeat][2];
-
+        
+        /* Begin rendering the comment with the details above */
         var commentPostDivEle = document.createElement("div");
         commentPostDivEle.className = "post";
 
@@ -81,7 +88,8 @@ if (amountOfComments > 0) { // if there were comments found
     }
 }
 
-else {
+else {  // if there were no comments found from the FLask response
+    /* Display a red text message to indicate no comments */
     var errorMessage = document.createElement("h1");
     errorMessage.style.color = "red";
     errorMessage.innerHTML = "No Comments";
